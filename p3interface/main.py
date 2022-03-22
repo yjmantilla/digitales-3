baba78685e55ddc95fecc0f359c0bcb61e5a0967
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-03-22 14:58:00 -0500

    buttons

--- p3interface/main.py
+++ p3interface/main.py
@@ -9,6 +9,28 @@ class TheApp(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # EVENTS
+
+        self.ui.btn_connect.clicked.connect(self.connect)
+        self.ui.btn_send.clicked.connect(self.send_data)
+        self.ui.btn_refresh.clicked.connect(self.refresh)
+        self.ui.btn_clear.clicked.connect(self.clear)
+    def connect(self):
+        print('connecting...')
+
+    def send_data(self):
+        print('sending...')
+    
+    def read_data(self):
+        print("reading...")
+
+    def refresh(self):
+        print("refreshing...")
+
+    def clear(self):
+        print("cleaning...")
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = TheApp()
